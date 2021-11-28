dc5b4a5f82cd092ca45c3838506e2e0d7ab83cfd
Akira <morinaka.aki@gmail.com>
2021-11-28 19:41:13 -0300

    componente para renderizar slides

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { SlidesComponent } from './components/slides/slides.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SlidesComponent,
   ],
   imports: [
     BrowserModule,
     CoreModule,
-    FeaturesModule
+    FeaturesModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
