cec6054eefd302fada54628711687dc0dc580dfa
rojasjoaquin129 <54474050+rojasjoaquin129@users.noreply.github.com>
2021-12-06 10:55:09 -0300

    creacion de las peticiones patch y delate globales privadas (#27)

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { CKEditorModule } from "ng2-ckeditor";
-import { BackofficeModule } from "./backoffice/backoffice.module";
 import { PublicModule } from "./public/public.module";
+import { BackofficeModule } from "./backoffice/backoffice.module";
 
 
 @NgModule({
