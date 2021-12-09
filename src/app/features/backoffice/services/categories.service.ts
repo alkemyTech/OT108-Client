983eb40e8130d62219fe14eabd22a6e77277a75b
ManuelGonzalez007 <manuelgonzalez14812@gmail.com>
2021-12-09 12:40:29 -0300

    variable de entorno para categorias

--- src/app/features/backoffice/services/categories.service.ts
+++ src/app/features/backoffice/services/categories.service.ts
@@ -2,22 +2,30 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Categories } from "src/app/models/categories";
 import { PrivateApiService } from "./private-api.service";
+import { environment } from "src/app/features/backoffice/pages/categories/environment";
+
 
 @Injectable({
   providedIn: "root",
 })
 export class CategoriesService {
-  constructor(private http: PrivateApiService) {}
+  constructor(private http: PrivateApiService) {
+    console.log("log: ",environment)
+  }
 
   createCategory(categoria: Categories): Observable<any> {
-    return this.http.post("categories", categoria);
+    return this.http.post(environment.categories, categoria);
   }
 
   editCategory(id: string, category: Categories): Observable<any> {
-    return this.http.put("categories", category, id);
+    return this.http.put(environment.categories, category, id);
   }
 
   getCategory(id: string): Observable<any> {
-    return this.http.get("categories", id);
+    return this.http.get(environment.categories, id);
+  }
+
+  deleteCategory(id: string): Observable<any> {
+    return this.http.delete(environment.categories, id);
   }
 }
