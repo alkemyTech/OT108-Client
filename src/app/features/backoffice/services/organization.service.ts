ab927772581bc77105f614091259c8c4aa7c33e9
Sandra47 <sandra.larafigueroa23@gmail.com>
2021-12-10 18:01:00 -0500

    Almacenar datos en variables de entorno OT108-91

--- src/app/features/backoffice/services/organization.service.ts
+++ src/app/features/backoffice/services/organization.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Organization } from "src/app/models/organization";
+import { environment } from "../pages/organization-edit/environment";
 import { PrivateApiService } from "./private-api.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class OrganizationService {
+  environment=environment.organization;
   constructor(private http: PrivateApiService) {}
 
   update(organization: Organization, id: number): Observable<any> {
-    return this.http.put("organization", organization, id.toString());
+    return this.http.put(this.environment, organization, id.toString());
   }
   getOrganization(id: number): Observable<any> {
-    return this.http.get("organization", id.toString());
+    return this.http.get(this.environment, id.toString());
+  }
+  deleteOrganization(id: number): Observable<any>{
+    return this.http.delete(this.environment, id.toString());
   }
 }
