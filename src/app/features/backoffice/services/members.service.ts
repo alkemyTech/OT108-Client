ef38e084e4f6280674696e7faff4ae75aa369069
rojasjoaquin129 <54474050+rojasjoaquin129@users.noreply.github.com>
2021-12-11 20:54:13 -0300

    cambio de url en servicios de  members y en categorias por variable de entorno (#42)

--- src/app/features/backoffice/services/members.service.ts
+++ src/app/features/backoffice/services/members.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from "@angular/core";
 import { Members } from "src/app/models/members";
+import { environment } from "src/environments/environment";
 import { PrivateApiService } from "./private-api.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class MembersService {
+  url = environment.members;
   constructor(private https: PrivateApiService) {}
 
   create(members: Members) {
-    return this.https.post("members", members);
+    return this.https.post(this.url, members);
   }
   editMembers(members: Members, id: string) {
-    return this.https.put("members", members, id);
+    return this.https.put(this.url, members, id);
   }
 
   getMember(id: string) {
-    return this.https.get("members", id);
+    return this.https.get(this.url, id);
   }
 }
