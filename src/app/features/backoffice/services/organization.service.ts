6161355881dfd00971058b51042bbd339cd8e5d0
Sandra47 <sandra.larafigueroa23@gmail.com>
2021-12-11 20:53:50 -0500

    modificacion variable de entorno OT108-91

--- src/app/features/backoffice/services/organization.service.ts
+++ src/app/features/backoffice/services/organization.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Organization } from "src/app/models/organization";
-import { environment } from "../pages/organization-edit/environment";
+import { environment } from "src/environments/environment";
 import { PrivateApiService } from "./private-api.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class OrganizationService {
-  environment=environment.organization;
+  Url = environment.organization;
   constructor(private http: PrivateApiService) {}
 
   update(organization: Organization, id: number): Observable<any> {
-    return this.http.put(this.environment, organization, id.toString());
+    return this.http.put(this.Url, organization, id.toString());
   }
   getOrganization(id: number): Observable<any> {
-    return this.http.get(this.environment, id.toString());
+    return this.http.get(this.Url, id.toString());
   }
   deleteOrganization(id: number): Observable<any>{
-    return this.http.delete(this.environment, id.toString());
+    return this.http.delete(this.Url, id.toString());
   }
 }
