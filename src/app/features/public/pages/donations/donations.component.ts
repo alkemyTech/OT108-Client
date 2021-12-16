75a47c0b1bec334174f8190418b3ef824209f285
ManuelGonzalez007 <manuelgonzalez14812@gmail.com>
2021-12-15 22:56:16 -0300

    Integrar Mercado Pago en screen donation

--- src/app/features/public/pages/donations/donations.component.ts
+++ src/app/features/public/pages/donations/donations.component.ts
@@ -1,26 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
 @Component({
   selector: "app-donations",
   templateUrl: "./donations.component.html",
   styleUrls: ["./donations.component.scss"],
 })
 export class DonationsComponent implements OnInit {
-  public formulario: FormGroup;
-  text: String = "Donacion a realizar";
-
-  constructor(private frB: FormBuilder) {
-    this.formulario = this.frB.group({
-      donation: ["", [Validators.required]],
-    });
-  }
-
+  constructor() {}
   ngOnInit(): void {}
-
-  donate() {
-    const donation = {
-      donation: "$" + this.formulario.get("donation")?.value,
-    };
-    console.log(donation);
-  }
 }
