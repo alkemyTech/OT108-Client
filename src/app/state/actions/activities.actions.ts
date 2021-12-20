967f31e8aafccb900aa3231eb88887fcd2f41bb7
joaquin <rojasjoaquin129@gmail.com>
2021-12-20 13:59:39 -0300

    cambio de servicio de alert , por dialog

--- src/app/state/actions/activities.actions.ts
+++ src/app/state/actions/activities.actions.ts
@@ -1,6 +1,6 @@
 import { createAction, props } from "@ngrx/store";
 import { Activity } from "src/app/models/activities";
-
+export const Activities_FAIL = "[Activities List/API] Activities fail";
 export const loadActivitiesList = createAction(
   "[Activities List/API] Load activities"
 );
@@ -8,3 +8,4 @@ export const retrievedActivitiesList = createAction(
   "[Activities List/API] Retrieve activities Success",
   props<{ items: ReadonlyArray<Activity> }>()
 );
+export const FailActivitiesList = createAction(Activities_FAIL);
