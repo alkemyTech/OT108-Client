e83c074aef8ca68f9aa4741f68e96973a8f4f484
Manuel González <manuelgonzalez14812@gmail.com>
2021-12-23 11:41:41 -0300

    Feat/ot108 144 (#62)
    * update package.json
    
    * Restringir acceso a formulario de Login para usuario autenticado

--- src/app/features/public/components/navbar/navbar.component.ts
+++ src/app/features/public/components/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
+  token: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    let token = localStorage.getItem("token");
+    if (token) {
+      this.token = true;
+    }
   }
-
 }
