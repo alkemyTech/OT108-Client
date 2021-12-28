b3c8f32d9b1fd0e192be5957366515b07372915d
Sandra47 <sandra.larafigueroa23@gmail.com>
2021-12-27 20:27:17 -0500

    Ultimas modificaciones OT108-137

--- src/app/features/public/components/components.module.ts
+++ src/app/features/public/components/components.module.ts
@@ -6,23 +6,17 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { TitlesComponent } from "./titles/titles.component";
 import { RouterModule } from "@angular/router";
 import { FooterComponent } from "src/app/components/footer/footer.component";
-<<<<<<< HEAD
 import { CountdownComponent } from './countdown/countdown.component';
 import { ComponentcampaignComponent } from './componentcampaign/componentcampaign.component';
-
-@NgModule({
-  declarations: [SlidesComponent, NavbarComponent,TitlesComponent, FooterComponent, CountdownComponent, ComponentcampaignComponent],
-  imports: [CommonModule, NgbModule,RouterModule,],
-=======
 import { DialogconfirmationComponent } from './dialogconfirmation/dialogconfirmation.component';
 import { NgxExtendedPdfViewerModule} from 'ngx-extended-pdf-viewer'
 import { MatDialogModule } from "@angular/material/dialog";
 
 
+
 @NgModule({
-  declarations: [SlidesComponent, NavbarComponent,TitlesComponent, FooterComponent, DialogconfirmationComponent],
+  declarations: [SlidesComponent, NavbarComponent,TitlesComponent, FooterComponent, DialogconfirmationComponent,CountdownComponent, ComponentcampaignComponent],
   imports: [CommonModule, NgbModule,RouterModule,NgxExtendedPdfViewerModule, MatDialogModule],
->>>>>>> main
   exports: [
     SlidesComponent,
     NavbarComponent,
