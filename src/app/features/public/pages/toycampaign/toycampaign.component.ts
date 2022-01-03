2f473d903ed08464c1705bd87d0851825dfa6a39
Sandra_Lara <75335174+Sandra47@users.noreply.github.com>
2022-01-03 15:29:28 -0500

    Modificacioncuentaregresiva (#76)
    * modificacion de la fecha de cuenta regresiva
    
    * modificacion de la fecha de cuenta regresiva
    
    * modificacion logo de campaña juguete

--- src/app/features/public/pages/toycampaign/toycampaign.component.ts
+++ src/app/features/public/pages/toycampaign/toycampaign.component.ts
@@ -13,7 +13,7 @@ export class ToycampaignComponent implements OnInit {
   descriptionCampa: string= "Regalemos sonrisa a un niño, Ayuda y apoya en donar un juquete a los niños que pertenecen a comunidades vunerables.  Únete en este mes a la campaña de juguetes, si todos nos unimos dando un granito de arena cambiaremos la vida de un niño que en medio de sus quehaceres y rutina será sorprendido con todo un show y regalos.";
   daysCampa:string= "Apartir del  Viernes 24 de enero de 10:00 a 15:00 Hrs. El lugar sera en nuestras Intalaciones de la de SOMOS TODOS";
   slogan:string = '"Esta navidad regalemos felicidad para los niños."';
-  logo:string = "../../../../../assets/images/corazon.jpg";
+  logo:string = "../../../../../assets/images/Logotipocampañajuguetes.png";
   load:boolean = true;
 
 
