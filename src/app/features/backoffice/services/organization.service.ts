67be46f4143a9d439be134fe544b0d3b862067b8
joaquin <rojasjoaquin129@gmail.com>
2022-01-03 16:17:51 -0300

    terminado

--- src/app/features/backoffice/services/organization.service.ts
+++ src/app/features/backoffice/services/organization.service.ts
@@ -11,13 +11,13 @@ export class OrganizationService {
   Url = environment.organization;
   constructor(private http: PrivateApiService) {}
 
-  update(organization: Organization, id: number){
+  update(organization: Organization, id: number) {
     return this.http.put(this.Url, organization, id.toString());
   }
-  getOrganization(id: number):Observable<any>{
+  getOrganization(id: number): Observable<any> {
     return this.http.get(this.Url, id.toString());
   }
-  deleteOrganization(id: number){
+  deleteOrganization(id: number) {
     return this.http.delete(this.Url, id.toString());
   }
 }
--- src/app/features/public/components/navbar/navbar.component.ts
+++ src/app/features/public/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { logOut } from "src/app/state/actions/auth.actions";
+import { AppState } from "src/app/state/app.state";
 
 @Component({
   selector: "app-navbar",
@@ -8,13 +11,16 @@ import { Component, OnInit,Input } from '@angular/core';
 export class NavbarComponent implements OnInit {
   token: boolean = false;
 
-  @Input() slogan:string = "";
-  @Input() campLogo:string = "";
-
-  constructor() { }
-
+  @Input() slogan: string = "";
+  @Input() campLogo: string = "";
+  email: string | null = "";
+  constructor(private store: Store<AppState>) {}
+  logOut() {
+    this.store.dispatch(logOut());
+  }
   ngOnInit(): void {
     let token = localStorage.getItem("token");
+    this.email = localStorage.getItem("email");
     if (token) {
       this.token = true;
     }
